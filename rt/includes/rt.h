8f594c5630392e013cd2cdb06f44cc083071fbd8
Leslie <dejagerleslie@gmail.com>
2022-10-22 17:29:59 +0200

    Move cylinder intersection to Rust

--- rt/includes/rt.h
+++ rt/includes/rt.h
@@ -285,7 +285,7 @@ int				intersection(t_obj obj, float *d, t_vector v, t_point n);
 // void			int_circle(t_objects obj, float *d, t_ray ray);
 // void			int_cone(t_objects obj, float *d, t_ray ray);
 void			int_cone_lid(t_ray obj, float *d, t_ray ray, float r);
-void			int_cyl(t_objects obj, float *d, t_ray ray);
+// void			int_cyl(t_objects obj, float *d, t_ray ray);
 void			int_cyl_lid(t_ray obj, float *d, t_ray ray, float r);
 void			int_plane(t_objects obj, float *d, t_ray ray);
 
--- rt_rs/src/cylinder.rs
+++ rt_rs/src/cylinder.rs
@@ -1,8 +1,8 @@
 use std::ffi::CStr;
 
-use libc::c_char;
+use libc::{c_char, c_float};
 
-use crate::{object::{ObjectItem, ObjectType}, data_extraction::{get_rad_h, get_reflect_refract, get_obj_options}, vec4_calc::convert_str_to_vec4, colour::convert_str_to_color};
+use crate::{object::{ObjectItem, ObjectType}, data_extraction::{get_rad_h, get_reflect_refract, get_obj_options}, vec4_calc::{convert_str_to_vec4, calc_dp, calc_unit_v, calc_p_to_v}, colour::convert_str_to_color, ray::{Quad, Ray}};
 
 #[no_mangle]
 pub unsafe extern "C" fn cylinder(obj: &mut ObjectItem, str: *const c_char) {
@@ -26,4 +26,26 @@ fn cylinder_extraction(str: String, obj: &mut ObjectItem) {
   get_reflect_refract(s.get(5).unwrap(), obj);
 	convert_str_to_color(s.get(6).unwrap().to_string(), &mut obj.col);
   get_obj_options(s.get(7).unwrap(), obj);
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn int_cyl(obj: ObjectItem, d: &mut c_float, ray: Ray) {
+  let mut cyl = Quad::default();
+	let dv = calc_dp(calc_unit_v(obj.dir), calc_unit_v(ray.v));
+	let co = calc_p_to_v(obj.c, ray.sc);
+	let cov = calc_dp(co, calc_unit_v(obj.dir));
+	cyl.a = 1. - dv * dv;
+	cyl.b = 2. * calc_dp(co, calc_unit_v(ray.v)) - 2. * dv * cov;
+	cyl.c = calc_dp(co, co) - cov * cov - obj.rad * obj.rad;
+	cyl.c = (cyl.b * cyl.b) - (4. * cyl.a * cyl.c);
+	if cyl.c > 0. {
+    cyl.t1 = (-cyl.b + cyl.c.sqrt()) / (2. * cyl.a);
+    cyl.t2 = (-cyl.b - cyl.c.sqrt()) / (2. * cyl.a);
+    if cyl.t1 >= 0. {
+      *d = cyl.t1;
+    }
+    if cyl.t2 >= 0. && (cyl.t2 < cyl.t1 || *d == -1.) {
+      *d = cyl.t2;
+    }
+  }
 }
