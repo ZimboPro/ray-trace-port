c1bed4682c3acf6b6a94675c3e522674b42fd97d
Leslie de Jager <leslie@bbd.co.za>
2022-11-28 14:46:52 +0200

    Fix binding

--- rt_rs/src/sequence.rs
+++ rt_rs/src/sequence.rs
@@ -1,12 +1,22 @@
-use std::{path::PathBuf, fs};
+use std::{path::PathBuf, fs, ffi::{CStr, c_char}};
 
 use crate::{world::{validate_file_contents}, object::objects, ray::raytrace};
 
-pub unsafe extern "C" fn sequence(file_name: &str) {
-  if validate_file_contents(file_name.into()) == 1 {
-    let path: PathBuf = file_name.into();
-    let contents = fs::read_to_string(path).unwrap();
-    let mut obj = objects(&contents);
-    raytrace(&mut obj);
-  }
+#[no_mangle]
+pub unsafe extern "C" fn sequence(val: *const c_char) {
+    if !val.is_null() {
+        let raw = CStr::from_ptr(val);
+
+        match raw.to_str() {
+          Ok(file_name) => {
+            if validate_file_contents(file_name.into()) == 1 {
+              let path: PathBuf = file_name.into();
+              let contents = fs::read_to_string(path).unwrap();
+              let mut obj = objects(&contents);
+              raytrace(&mut obj);
+            }
+          },
+          Err(e) => eprintln!("File name error: {}", e)
+        }
+    }
 }
