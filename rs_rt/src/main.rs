8bab1419e906f4b8fa3119634007709412984ecb
Leslie <dejagerleslie@gmail.com>
2022-12-02 22:29:26 +0200

    Fix cli args

--- rs_rt/src/main.rs
+++ rs_rt/src/main.rs
@@ -4,36 +4,40 @@ use rt_rs::{aa_seq, non_aa_seq};
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-   /// Name of the person to greet
-   #[arg(short, long)]
+   /// File name of the world
+   #[arg(short, long, )]
    file: String,
 
-   /// Number of times to greet
+   /// Enable antialiasing
    #[arg(short, long)]
-   aa: Option<bool>,
+   aa: bool,
 }
 
 
 fn main() {
     let len = std::env::args().len();
     let args = Args::parse();
-    if len != 2 && len != 3
-	{
-		println!("Usage ./RTv1 map");
-		println!("OR");
-		println!("Usage ./RTv1 map -a");
-	}
-	else
-	{
-		match args.aa {
-				Some(aliasing) => {
-					if aliasing {
-						aa_seq(&args.file);
-					} else {
-						non_aa_seq(&args.file);
-					}
-				},
-				None => non_aa_seq(&args.file)
+		// println!(le)
+  //   if len != 2 && len != 3
+	// {
+	// 	println!("Usage ./RTv1 map");
+	// 	println!("OR");
+	// 	println!("Usage ./RTv1 map -a");
+	// }
+	// else
+	// {
+		if args.aa {
+			aa_seq(&args.file);
+		} else {
+			non_aa_seq(&args.file);
 		}
-	}
+		// match args.aa {
+		// 		Some(aliasing) => {
+		// 			if aliasing {
+		// 			} else {
+		// 			}
+		// 		},
+		// 		None => non_aa_seq(&args.file)
+		// }
+	// }
 }
